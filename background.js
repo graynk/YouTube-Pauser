ddd6b383f62613b02cb5a0868e4d660d0f17a7f6
graynk <gray.nk@gmail.com>
2022-04-19 18:41:29 +0500

    added icon/title changing, removed unneeded stuff

--- background.js
+++ background.js
@@ -1,10 +1,23 @@
-// Current state to which we will sync tabs to. TODO: add an icon
+// Current state to which we will sync tabs to, when clicked.
 let play = true;
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	// Send a message to the active tab
-	chrome.tabs.query({url: '*://www.youtube.com/watch?v*'}, function(tabs) { // Gets all tabs with youtube playing a video
-		play = !play
+	// Gets all tabs with YouTube playing a video
+	chrome.tabs.query({url: '*://www.youtube.com/watch?v*'}, function(tabs) {
+		if (tabs.length === 0) {
+			return;
+		}
+
+		const icon = play ? 'icons/play.png' : 'icons/pause.png';
+		const title = play ? 'Play YouTube' : 'Pause YouTube';
+		play = !play;
+
+		chrome.browserAction.setIcon({
+			path: icon,
+		});
+		chrome.browserAction.setTitle({
+			title: title,
+		});
 		for (let tab of tabs) {
 			chrome.tabs.sendMessage(tab.id, {'message': 'toggle_playing', 'play': play})
 		}
--- content.js
+++ content.js
@@ -1,18 +1,12 @@
-// content.js
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
+		if (request.message !== 'toggle_playing') return;
+
 		const video = document.getElementsByTagName('video')[0];
-		switch (request.message) {
-			case 'toggle_playing':
-				if (request.play && video.paused) {
-					video.play();
-				} else if (!request.play && !video.paused) {
-					video.pause();
-				}
-				break;
-			// TODO: add pop-up allowing to select active tabs to play/pause, receive info via request
-			case 'request_video_data':
-				sendResponse({'title': video.title});
+		if (request.play && video.paused) {
+			video.play();
+		} else if (!request.play && !video.paused) {
+			video.pause();
 		}
   	}
 );
